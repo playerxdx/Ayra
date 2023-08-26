82d6af6d681a4c879b08fc24db0ea4efcf1fcea0
playerxdx <128796714+playerxdx@users.noreply.github.com>
2023-08-26 18:08:04 +0530

    Update __init__.py

--- tg_bot/__init__.py
+++ tg_bot/__init__.py
@@ -83,7 +83,7 @@ class KigyoINIT:
     def __init__(self, parser: ConfigParser):
         self.parser = parser
         self.SYS_ADMIN: int = self.parser.getint('SYS_ADMIN', '0')
-        self.OWNER_ID: int = self.parser.getint('OWNER_ID', '6366909726')
+        self.OWNER_ID: int = self.parser.getint('OWNER_ID', '0')
         self.OWNER_USERNAME: str = self.parser.get('OWNER_USERNAME', "WonderAakash")
         self.APP_ID: str = self.parser.getint("APP_ID")
         self.API_HASH: str = self.parser.get("API_HASH")
